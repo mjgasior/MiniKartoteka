c5d4286c87b277237cd2ca823de648e397b5c06d
Michał Józef Gąsior <michaljgasior@outlook.com>
2016-03-26 23:48:52 +0100

    Set up nHibernate

---
+++ Data/MiniKartoteka.Data.PatientDAL/NHibernateConfigurator.cs
@@ -0,0 +1,34 @@
+﻿using MiniKartoteka.Data.DataBaseModels;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using System.Reflection;
+
+namespace MiniKartoteka.Data.PatientDAL
+{
+    public static class NHibernateConfigurator
+    {
+        public static void Configurate()
+        {
+            var cfg = new Configuration();
+            cfg.DataBaseIntegration(x => {
+                x.ConnectionString = "Server=localhost;Database=NHibernateDemo;Integrated Security=SSPI;";
+                x.Driver<NpgsqlDriver>();
+                x.Dialect<PostgreSQLDialect>();
+                });
+            cfg.AddAssembly(Assembly.GetAssembly(typeof(Wwywalto)));
+            ISessionFactory sessionFactory = cfg.BuildSessionFactory();
+
+            using (ISession session = sessionFactory.OpenSession())
+            {
+                using (ITransaction transaction = session.BeginTransaction())
+                {
+                    // perfrom database logic
+
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}
