a8f24b824e814614213ff2bb961aca393eaeb7e8
Michał Józef Gąsior <michaljgasior@outlook.com>
2016-03-26 16:44:47 +0100

    Mastering TabControl - end

--- Modules/MiniKartoteka.Modules.PatientsModule/ViewModels/PatientsContentViewModel.cs
+++ Modules/MiniKartoteka.Modules.PatientsModule/ViewModels/PatientsContentViewModel.cs
@@ -1,8 +1,15 @@
 ﻿using MiniKartoteka.Modules.PatientsModule.Abstract.ViewModels;
+using Prism.Mvvm;
 
 namespace MiniKartoteka.Modules.PatientsModule.ViewModels
 {
-    public class PatientsContentViewModel : IPatientsContentViewModel
+    public class PatientsContentViewModel : BindableBase, IPatientsContentViewModel
     {
+        public string Title { get; set; }
+
+        public PatientsContentViewModel()
+        {
+            Title = "Tytuł";
+        }
     }
 }
